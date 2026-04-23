4e2c829207d2dcc666753157ba28da8d5b966ea2
moonnasir394-commits <moonnasir394@gmail.com>
2026-04-24 00:25:04 +0500

    Update astronomy_bot_backend.py

--- astronomy_bot_backend.py
+++ astronomy_bot_backend.py
@@ -48,4 +48,4 @@ def chat():
         print("ERROR:", e)
         return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
